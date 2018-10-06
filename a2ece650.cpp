6090bc1c09f1d7d17315ee8bbe9424c05f68ec5e
Adam Kulas <adam.kulas1@gmail.com>
2018-10-06 01:49:15 -0400

    basic loop to interpret commands

--- a2ece650.cpp
+++ a2ece650.cpp
@@ -1,16 +1,49 @@
-// compile: c++ -o complex_main complex_main.cpp
+// compile: g++ -o a2ece650 ../a2ece650.cpp
 #include <iostream>
 #include "a2ece650.hpp"
 
 int main() {
-    Complex cx(10, 4);
+    // Complex cx(10, 4);
 
-    std::cout << "Absolute value of " << cx.real() << "+" << cx.imag() << "i"
-              << " is " << cx.abs() << "\n";
-    
-    int firstInt;
-    int secondInt;
-    std::cin >> firstInt >> secondInt;
-    std::cout << firstInt << secondInt << std::endl;
+    // std::cout << "Absolute value of " << cx.real() << "+" << cx.imag() << "i"
+    //           << " is " << cx.abs() << "\n";
+
+    char cmd;
+    int vertices;
+    int vertexStart;
+    int vertexEnd;
+    std::string edges;
+
+    std::cout << "Program Start" << std::endl;
+    while(std::cin >> cmd){    
+        //std::cin >> cmd;  
+        switch(cmd) {
+            case 'V': case 'v':
+                std::cin >> vertices;
+                std::cout << "Vertex cmd entered " << vertices << std::endl;
+                break;
+            
+            case 'E': case 'e':
+                std::cin >> edges;
+                std::cout << "Edge cmd entered " << edges << std::endl;
+                break;
+
+            case 'S': case 's':
+                std::cin >> vertexStart >> vertexEnd;
+                std::cout << "Shortest cmd entered " << vertexStart << ' ' << vertexEnd << std::endl;
+                break;
+
+            default:
+                std::cerr << "Error: command not recognized" << std::endl;
+        }
+    }
+
+    // std::string line;
+    // while (std::getline(std::cin, line)) //Take input until EOF character is found
+    //     {
+    //         std::cout << line << std::endl;
+    //     }
+    std::cout << "This is the end" << std::endl;
+
     return 0;
 }
